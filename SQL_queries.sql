3a8a7972b20e1aee0d9362667ebb0b3b4f359613
Mayor Chiamaka Oghenekeno <mayorchiamaka@gmail.com>
2024-11-01 02:43:45 -0700

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,1 +1,153 @@
 
+-- Data cleaning
+SELECT
+	*
+FROM sales;
+
+
+-- Add the time_of_day column
+SELECT
+	time,
+	(CASE
+		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+        ELSE "Evening"
+    END) AS time_of_day
+FROM sales;
+
+
+ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
+
+-- For this to work turn off safe mode for update
+-- Edit > Preferences > SQL Edito > scroll down and toggle safe mode
+-- Reconnect to MySQL: Query > Reconnect to server
+UPDATE sales
+SET time_of_day = (
+	CASE
+		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
+        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
+        ELSE "Evening"
+    END
+);
+
+
+-- Add day_name column
+SELECT
+	date,
+	DAYNAME(date)
+FROM sales;
+
+ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
+
+UPDATE sales
+SET day_name = DAYNAME(date);
+
+
+-- Add month_name column
+SELECT
+	date,
+	MONTHNAME(date)
+FROM sales;
+
+ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
+
+UPDATE sales
+SET month_name = MONTHNAME(date);
+
+-- ------------------------------------------------------------------
+-- -------------------Generic----------------------------------------
+-- ------------------------------------------------------------------
+
+-- How many unique cities does the data have?
+SELECT DISTINCT city
+FROM sales;
+
+-- In which cities is each branch?
+SELECT DISTINCT city, branch
+FROM sales
+
+-- ------------------------------------------------------------
+-- -------------Product----------------------------------------
+-- -------------------------------------------------------------
+
+-- What is the most selling product?
+SELECT SUM(quantity) AS qty, product_line
+FROM sales
+GROUP BY product_line
+ORDER BY qty DESC;
+
+-- What is the common product line by gender?
+SELECT gender, product_line, COUNT(gender) AS total_cnt
+FROM sales
+GROUP BY gender, product_line
+ORDER BY total_cnt DESC;
+
+-- What product line had the largest revenue?
+SELECT
+	product_line,
+	SUM(total) as total_revenue
+FROM sales
+GROUP BY product_line
+ORDER BY total_revenue DESC;
+
+-- What is the total revenue by month
+SELECT
+	month_name AS month,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY month_name 
+ORDER BY total_revenue;
+
+-- ----------------------------------------------
+-- --------------Customer------------------------
+-- ----------------------------------------------
+
+-- How many unique customer types does the data have?
+SELECT
+	DISTINCT customer_type
+FROM sales;
+
+-- How many unique customer types does the data have?
+SELECT
+	DISTINCT customer_type
+FROM sales;
+
+-- What is the most common customer type?
+SELECT
+	customer_type,
+	count(*) as count
+FROM sales
+GROUP BY customer_type
+ORDER BY count DESC;
+
+-- ------------------------------------------
+-- -------------Sales------------------------
+-- ------------------------------------------
+
+-- Number of sales made in each time of the day per weekday 
+SELECT
+	time_of_day,
+	COUNT(*) AS total_sales
+FROM sales
+WHERE day_name = "Sunday"
+GROUP BY time_of_day 
+ORDER BY total_sales DESC;
+-- Evenings experience most sales, the stores are 
+-- filled during the evening hours
+
+-- Which of the customer types brings the most revenue?
+SELECT
+	customer_type,
+	SUM(total) AS total_revenue
+FROM sales
+GROUP BY customer_type
+ORDER BY total_revenue;
+
+-- Which city has the largest tax/VAT percent?
+SELECT
+	city,
+    ROUND(AVG(tax_pct), 2) AS avg_tax_pct
+FROM sales
+GROUP BY city 
+ORDER BY avg_tax_pct DESC;
+
